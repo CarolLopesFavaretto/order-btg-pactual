6931a9b2caf57a2d02abb5308abee4f3d640fb1b
Caroline <carolinelopesrdg@hotmail.com>
2024-08-18 19:15:37 -0300

    mapeando entidades;

--- src/main/java/com/btgpactual/order/Entity/Order.java
+++ src/main/java/com/btgpactual/order/Entity/Order.java
@@ -3,7 +3,10 @@ package com.btgpactual.order.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.math.BigDecimal;
@@ -18,8 +21,10 @@ public class Order {
     @MongoId
     private Long id;
 
+    @Indexed(name = "customer_id_index")
     private Long customerId;
 
+    @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal amount;
 
     private List<OrderItem> items;
--- src/main/java/com/btgpactual/order/Entity/OrderItem.java
+++ src/main/java/com/btgpactual/order/Entity/OrderItem.java
@@ -3,6 +3,8 @@ package com.btgpactual.order.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.math.BigDecimal;
 @Data
@@ -12,5 +14,6 @@ public class OrderItem {
 
     private String product;
     private Integer quantity;
+    @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal price;
 }
